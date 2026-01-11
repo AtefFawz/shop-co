97b87c7df961c1cec34fa4a331675b8d8ded7420
Atef fawzy <ateffawzy897@gmail.com>
2026-01-11 14:00:08 +0200

    refactor: clean up comments and improve code readability in filterStore

--- src/store/filterStore.tsx
+++ src/store/filterStore.tsx
@@ -1,13 +1,13 @@
 import { create } from "zustand";
 import { Product } from "@/types";
-import { products as allProducts } from "@/data/data"; // الداتا الأصلية
+import { products as allProducts } from "@/data/data";
 
 interface FilterStore {
   products: Product[];
-  currentType: string | null; // 👈 دي الذاكرة الجديدة (male, female, etc)
+  currentType: string | null;
 
-  filterByType: (type: string) => void; // دي اللي بتتنادى من صفحة Category
-  filterBySection: (section: string) => void; // دي اللي بتتنادى من الـ Sidebar
+  filterByType: (type: string) => void; //
+  filterBySection: (section: string) => void;
   resetFilter: () => void;
   filterPrice: (maxPrice: number) => void;
   // sec: string | null;
@@ -15,37 +15,28 @@ interface FilterStore {
 
 export const useFilterStore = create<FilterStore>()((set, get) => ({
   products: allProducts,
-  currentType: null, // في البداية مفيش نوع محدد
-  // sec: null,
-  // 1. لما يدوس على كارت في صفحة Category (Men, Women)
+  currentType: null,
   filterByType: (type) => {
     set({
-      currentType: type, // ✅ احفظ النوع في الذاكرة
-      products: allProducts.filter((item) => item.type === type), // فلتر المنتجات عليه
+      currentType: type,
+      products: allProducts.filter((item) => item.type === type),
     });
   },
 
-  // 2. لما يختار من السايد بار (T-shirt, Jeans)
   filterBySection: (section) => {
-    // هات الذاكرة الحالية
     const { currentType } = get();
 
-    // ابدأ الفلترة من الداتا الأصلية
     let filtered = allProducts;
 
-    // خطوة 1: لو فيه نوع محفوظ (مثلاً رجالي)، طبق الفلتر بتاعه الأول
     if (currentType) {
       filtered = filtered.filter((item) => item.type === currentType);
     }
 
-    // خطوة 2: طبق فلتر القسم (التيشرتات)
     filtered = filtered.filter((item) => item.section === section);
 
-    // حدث النتيجة
     set({ products: filtered });
   },
 
-  // 3. فلتر السعر (لازم يحترم الفلاتر السابقة برضه)
   filterPrice: (maxPrice) => {
     const { currentType } = get();
     let filtered = allProducts;
@@ -53,14 +44,12 @@ export const useFilterStore = create<FilterStore>()((set, get) => ({
       filtered = filtered.filter((item) => item.type === currentType);
     }
 
-    // هنا ممكن تحتاج تحتفظ بـ currentSection كمان لو عايز دقة 100%
-    // بس مبدئياً ده هيجيبلك الحاجات اللي في سعرك تبع النوع المختار
     filtered = filtered.filter((item) => item.price <= maxPrice);
 
     set({ products: filtered });
   },
 
   resetFilter: () => {
-    set({ products: allProducts, currentType: null }); // فضي الذاكرة ورجع كل حاجة
+    set({ products: allProducts, currentType: null });
   },
 }));
