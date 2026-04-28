c1e8583edb8314e027e3771b6dffe94bc53de47b
Atef fawzy <ateffawzy897@gmail.com>
2026-04-28 04:57:59 +0300

    fix: ensure correct baseURL format in API configuration

--- src/lib/serverApi.ts
+++ src/lib/serverApi.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/",
 });
 const serverApi = async (url: string) => {
   const token = (await cookies()).get("token")?.value;
